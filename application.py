2682a99e8ef6b2767afdd571e2100bfdd1aa60f8
Swadhin Gyanajyoti Nayak <117260349+Swadhin-203@users.noreply.github.com>
2023-05-11 01:11:45 +0530

    Update application.py
    Showing the Form in the starting of the application

--- application.py
+++ application.py
@@ -6,11 +6,8 @@ application=Flask(__name__)
 
 app=application
 
-@app.route('/')
-def home_page():
-    return render_template('index.html')
 
-@app.route('/predict',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 
 def predict_datapoint():
     if request.method=='GET':
